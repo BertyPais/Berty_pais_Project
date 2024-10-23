30cea7b4eaa4afda94df24918f440df0a472a179
Sana <sanayaseer@gmail.com>
2024-10-23 14:59:08 +0530

    This is my new Project

---
+++ Git.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Git 
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int n=nextInt();
+		System.out.println(n);
+	}
+}
